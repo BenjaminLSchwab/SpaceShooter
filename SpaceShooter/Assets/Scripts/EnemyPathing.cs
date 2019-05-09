10bb46627b3a66fbc49f74803cba5096de2f9f2b
Ben <benjamin.schwab.nonspam@gmail.com>
2019-05-09 15:26:02 -0700

    enemy moves along path

--- SpaceShooter/Assets/Scripts/EnemyPathing.cs
+++ SpaceShooter/Assets/Scripts/EnemyPathing.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField]
-    List<Transform> waypoints = new List<Transform>();
+    [SerializeField] List<Transform> waypoints = new List<Transform>();
+    [SerializeField] float moveSpeed = 2f;
+    int waypointIndex = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = waypoints[0].position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].position;
+            var movementThisFrame = Time.deltaTime * moveSpeed;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }

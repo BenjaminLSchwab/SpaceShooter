467470bd31b8de341f278665844e9ca4f0b4e0e2
Ben <benjamin.schwab.nonspam@gmail.com>
2019-05-08 18:27:56 -0700

    adding enemy path

---
+++ SpaceShooter/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField]
+    List<Transform> waypoints = new List<Transform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

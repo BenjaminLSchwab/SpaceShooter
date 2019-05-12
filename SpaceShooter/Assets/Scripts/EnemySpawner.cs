fe567845cf9e873912b270d9da7add02b427d1b1
Ben <benjamin.schwab.nonspam@gmail.com>
2019-05-12 16:48:51 -0700

    multiple enemies spawn in wave

---
+++ SpaceShooter/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    int startingWave = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    // Update is called once per frame
+private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int i = 0; i < waveConfig.GetNumberOfEnemies(); i++)
+        {
+        Instantiate(
+            waveConfig.GetEnemyPrefab(),
+            waveConfig.GetWaypoints()[0].position,
+            Quaternion.identity
+            );
+        yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+    }
+}

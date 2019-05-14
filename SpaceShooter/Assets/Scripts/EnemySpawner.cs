68752402ab49e7cf8b357465d48e1be66a638ffe
Ben <benjamin.schwab.nonspam@gmail.com>
2019-05-14 13:41:46 -0700

    multi waves

--- SpaceShooter/Assets/Scripts/EnemySpawner.cs
+++ SpaceShooter/Assets/Scripts/EnemySpawner.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] List<WaveConfig> waveConfigs;
-    int startingWave = 0;
+    [SerializeField] int startingWave = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        var currentWave = waveConfigs[startingWave];
-        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        StartCoroutine(SpawnAllWaves());
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        for (int waveIndex = startingWave; waveIndex < waveConfigs.Count; waveIndex++ )
+        {
+            yield return SpawnAllEnemiesInWave(waveConfigs[waveIndex]);
+        }
     }
 
     // Update is called once per frame
-private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
     {
         for (int i = 0; i < waveConfig.GetNumberOfEnemies(); i++)
         {

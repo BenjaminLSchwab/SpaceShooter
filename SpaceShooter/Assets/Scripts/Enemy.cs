e5df7156f918bb8c93f8fb2610115e637532a574
Ben <benjamin.schwab.nonspam@gmail.com>
2019-05-20 18:11:44 -0700

    enemies can shoot now

--- SpaceShooter/Assets/Scripts/Enemy.cs
+++ SpaceShooter/Assets/Scripts/Enemy.cs
@@ -1,20 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float health = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimebetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] float laserSpawnDistance = .1f;
+    [SerializeField] float laserSpeed = 10f;
+    [SerializeField] GameObject laserPrefab;
+    List<GameObject> laserPool = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCounter = UnityEngine.Random.Range(minTimebetweenShots, maxTimeBetweenShots);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter < 0)
+        {
+            Fire();
+            shotCounter = UnityEngine.Random.Range(minTimebetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        var laserSpawnPos = transform.position + new Vector3(0, -laserSpawnDistance, -1);
+        var laser = FindFirstInactiveLaser();
+        if (laser == null)
+        {
+            laser = Instantiate(laserPrefab, laserSpawnPos, Quaternion.identity);
+            laserPool.Add(laser);
+        }
+        else
+        {
+            laser.SetActive(true);
+            laser.transform.position = laserSpawnPos;
+        }
+        var rb = laser.GetComponent<Rigidbody2D>();
+        var vel = Vector2.up * -laserSpeed;
+        rb.velocity = vel;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -28,5 +65,18 @@ public class Enemy : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    GameObject FindFirstInactiveLaser()
+    {
+        if (laserPool.Count == 0)
+        {
+            return null;
+        }
+        foreach (GameObject laser in laserPool)
+        {
+            if (!laser.activeInHierarchy) return laser;
+        }
+        return null;
     }
 }

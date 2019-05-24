1f6fa199f83a9d53845ac1e28e9a1b18dfc53829
Ben <benjamin.schwab.nonspam@gmail.com>
2019-05-24 15:50:38 -0700

    scrolling background

---
+++ SpaceShooter/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] float scrollSpeed = 0.5f;
+    Material material;
+    Vector2 offset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        offset = new Vector2(0, scrollSpeed);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.mainTextureOffset += offset * Time.deltaTime;
+    }
+}

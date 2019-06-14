9659ef7bc59f3d3373bdf57a055fec9697db8919
Ben <benjamin.schwab.nonspam@gmail.com>
2019-06-13 18:45:54 -0700

    added menu and game over screen

---
+++ SpaceShooter/Assets/Scripts/Level.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Level_01");
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}

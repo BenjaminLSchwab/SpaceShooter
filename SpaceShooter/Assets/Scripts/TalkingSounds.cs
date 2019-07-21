bc5de24a814bb1054a225ac8c2435e151e47c64c
Ben <benjamin.schwab.nonspam@gmail.com>
2019-07-21 12:48:05 -0700

    talking sounds proof of concept

---
+++ SpaceShooter/Assets/Scripts/TalkingSounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkingSounds : MonoBehaviour
+{
+    [SerializeField] List<AudioClip> audioClips;
+    [SerializeField] [Range(0,1)] float volume = 1f;
+    [SerializeField] float delayBetweenClips = 0.65f;
+
+    private void OnEnable()
+    {
+        StartCoroutine(PlaySounds());
+    }
+
+    
+
+    IEnumerator PlaySounds()
+    {
+        while (true)
+        {
+            var currentClip = audioClips[Random.Range(0, audioClips.Count)];
+            AudioSource.PlayClipAtPoint(currentClip, transform.position, volume);
+            yield return new WaitForSeconds(delayBetweenClips);
+        }
+    }
+}

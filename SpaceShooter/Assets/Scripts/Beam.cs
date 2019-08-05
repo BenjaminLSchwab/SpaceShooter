b2697f846d01fe8dfb6b7e9ce1ac2365f04e4132
Ben <benjamin.schwab.nonspam@gmail.com>
2019-08-05 15:24:30 -0700

    rough laser enemy

--- SpaceShooter/Assets/Scripts/Beam.cs
+++ SpaceShooter/Assets/Scripts/Beam.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 
 public class Beam : MonoBehaviour
 {
-    [SerializeField] GameObject warningBeamSegment;
     [SerializeField] GameObject beamSegment;
     [SerializeField] GameObject beamEndPieceTemplate;
-    [SerializeField] float spaceBetweenSegments = 1f;
-    [SerializeField] float maxRange = 20f;
+    [SerializeField] float offSet = 0.3f;
+    [SerializeField] float spaceBetweenSegments = 0.5f;
+    [SerializeField] float maxRange = 40f;
+    [SerializeField] bool beamOn = false;
+    [SerializeField] float beamSpeed = 30f;
+    [SerializeField] float beamToggleTime = 0.5f;
+    [SerializeField][Range(0,1)] float beamOnRate = 0.5f;
+
+
 
     List<GameObject> beamSegments = new List<GameObject>();
     GameObject beamEndPiece;
-    bool beamOn = false;
-
+    bool clearedSegments = false;
+    float currentBeamLength = 0f;
+    float beamTimer = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -22,10 +29,55 @@ public class Beam : MonoBehaviour
         
     }
 
+    void ToggleBeam()
+    {
+        if (beamOn)
+        {
+            beamOn = false;
+        }
+        else
+        {
+            beamOn = true;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        UpdateBeam();
+        beamTimer += Time.deltaTime;
+        if (beamTimer > beamToggleTime)
+        {
+            beamTimer = 0;
+            beamOn = false;
+        }
+        if (beamTimer < Mathf.Lerp(0, beamToggleTime, beamOnRate))
+        {
+            beamOn = true;
+        }
+        if (beamOn && clearedSegments == false)
+        {
+            if (currentBeamLength < maxRange)
+            {
+                currentBeamLength += Time.deltaTime * beamSpeed;
+            }
+            UpdateBeam();
+            clearedSegments = false;
+        }
+        else if (beamOn)
+        {
+            currentBeamLength = 0;
+            clearedSegments = false;
+        }
+        else
+        {
+            foreach (var segment in beamSegments)
+            {
+                segment.SetActive(false);
+            }
+            if (beamEndPiece) { beamEndPiece.SetActive(false); }
+            clearedSegments = true;
+        }
+        
     }
 
     GameObject FindFirstInactiveBeamSegment()
@@ -49,43 +101,47 @@ public class Beam : MonoBehaviour
         }
         if (beamEndPiece) { beamEndPiece.SetActive(false); }
 
-        var hit = Physics2D.Raycast(transform.position, transform.up, maxRange);
-        var endPoint = transform.up * maxRange; 
+
+        var hit = Physics2D.Raycast(transform.position, -transform.up, currentBeamLength);
+        var endPoint = -transform.up * currentBeamLength; 
         if (hit.collider)
         {
-        endPoint = hit.transform.position;
-            if (!beamEndPiece)
+            if (hit.transform.gameObject.layer != 9 && hit.transform.gameObject.layer != 10 && hit.transform.gameObject.layer != 11)
             {
-                beamEndPiece = Instantiate(beamEndPieceTemplate, endPoint, transform.rotation);
+                endPoint = hit.point;
+                if (!beamEndPiece)
+                {
+                    beamEndPiece = Instantiate(beamEndPieceTemplate, endPoint, transform.rotation);
+                }
+                else
+                {
+                    beamEndPiece.SetActive(true);
+                    beamEndPiece.transform.position = endPoint;
+                    beamEndPiece.transform.rotation = transform.rotation;
+                }
             }
-            else
-            {
-                beamEndPiece.SetActive(true);
-                beamEndPiece.transform.position = endPoint;
-                beamEndPiece.transform.rotation = transform.rotation;
-            }
+
         }
 
         var lengthOfBeam = (endPoint - transform.position).magnitude;
-        //if (beamOn)
-        //{
+
             for (int i = 0; i < lengthOfBeam; i++)
             {
             var segment = FindFirstInactiveBeamSegment();
+            var pos = (transform.position + (-transform.up * i * spaceBetweenSegments) + (-transform.up * offSet));
             if (!segment)
             {
-                segment = Instantiate(beamSegment, (transform.position + transform.up * i), transform.rotation);
+                segment = Instantiate(beamSegment, pos, transform.rotation);
                 beamSegments.Add(segment);
 
             }
             else
             {
                 segment.SetActive(true);
-                segment.transform.position = transform.position + transform.up * i;
+                segment.transform.position = pos;
                 segment.transform.rotation = transform.rotation;
             }
 
             }
-        //}
-    }
+        }
 }
---
+++ SpaceShooter/Assets/Scripts/Enemy/AimAtPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimAtPlayer : MonoBehaviour
+{
+    [SerializeField] protected float aimSpeed = 0.1f;
+    protected GameObject player;
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TurnTowardTarget();
+    }
+
+    protected void TurnTowardTarget()
+    {
+        Quaternion facingPlayer = Quaternion.LookRotation(Vector3.forward, (transform.position - player.transform.position).normalized);
+        transform.rotation = Quaternion.Lerp(transform.rotation, facingPlayer, Time.deltaTime * aimSpeed);
+
+    }
+
+
+}
